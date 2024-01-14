574376cc73d9c331ba42aca9ed6a3f7e0e47bf00
bennzou <molmaraid.chainsou.cord62045@gmail.com>
2024-01-14 19:04:03 +0900

    issueの再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -17,6 +17,8 @@ class CalendarsController < ApplicationController
   def plan_params
     params.require(:plan).permit(:date,:plan)
   end
+
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -31,12 +33,15 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      
+
       wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
       if wday_num >= 7 #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays:wdays[(@todays_date+x).wday]}
       @week_days.push(days)
     end
+
   end
 end
